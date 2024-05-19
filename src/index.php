fc57b1c9124858b5d98063949e9525f3b8d4e086
DAH5 <contact@dah5.me.uk>
2024-05-19 13:50:24 +0100

    Fixed the destinations of the buttons on the homepage.

--- src/index.php
+++ src/index.php
@@ -23,14 +23,14 @@ layout: src/_inc/base.php
             <div style="aspect-ratio: 16 / 9; background-size: cover; background-position: center; background-image: url( '<?php echo IMAGES_URL; ?>/logos/StaticPHP-Promo1.png' );"></div>
             <h2 class="w3-xlarge"><b>StaticPHP</b></h2>
             <p class="w3-padding">A Static Site Generator written in PHP. It turns source PHP files into static HTML files.</p>
-            <p><a href="/analogueradio" class="w3-button w3-round w3-dark-gray"><b>Visit Site</b></a></p>
+            <p><a href="/staticphp" class="w3-button w3-round w3-dark-gray"><b>Visit Site</b></a></p>
         </div>
 
         <div class="w3-third w3-center">
             <div style="aspect-ratio: 16 / 9; background-size: cover; background-position: center; background-image: url( '<?php echo IMAGES_URL; ?>/ui/transparent-16-9.png' );"></div>
             <h2 class="w3-xlarge"><b>Staticly</b></h2>
             <p class="w3-padding">The essential repository for Front End Web Developers.</p>
-            <p><a href="/analogueradio" class="w3-button w3-round w3-dark-gray"><b>Visit Site</b></a></p>
+            <p><a href="/staticly" class="w3-button w3-round w3-dark-gray"><b>Visit Site</b></a></p>
         </div>
     </div>
 </section>
