6e84fed9bf46e8a6f2eee8994c6cce63340bb720
DAH5 <contact@dah5.me.uk>
2025-07-23 18:29:16 +0100

    Added a fun teapot page

---
+++ src/teapot.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<title>418 I'm a teapot</title>
+		
+		<style type="text/css">
+			html
+			{
+				background: #fff;
+				color: #222;
+				padding: 15px;
+			}
+			
+			body
+			{
+				margin-left: auto;
+				margin-right: auto;
+				max-width: 650px;
+				font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, "Helvetica Neue", Arial, sans-serif;
+			}
+			
+			ins
+			{
+				color: #777;
+				text-decoration: none;
+			}
+			
+			img
+			{
+				max-width: 100%;
+			}
+			
+			.teapot
+			{
+				width: 100%;
+				max-height: 248px;
+				text-align: center;
+			}
+			
+			.teapot img
+			{
+				max-width: 278px;
+			}
+			
+			@media ( min-width: 650px )
+			{
+				body
+				{
+					display: flex;
+					flex-direction: row;
+					margin-top: 128px;
+				}
+				
+				.teapot
+				{
+					width: 278px;
+				}
+			}
+		</style>
+	</head>
+	
+	<body>
+		<div class="content">
+			<p><img src="/assets/images/logos/dah5-logo-2024-alternate-150x35.png"></p>
+			
+			<p><b>418.</b>&nbsp;<ins>I'm a teapot</ins></p>
+			
+			<p>The requested resource has a handle and a spout.<br><ins>Try tipping it over and pouring it out.</ins></p>
+		</div>
+		
+		<div class="teapot">
+			<p><img src="/assets/images/teapot.png"></p>
+		</div>
+	</body>
+</html>
